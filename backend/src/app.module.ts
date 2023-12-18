0976cdf7f66da2b55118fb317ae0934cb538dc9e
Alan SCHNEGG <alan1.schnegg@epitech.eu>
2023-12-18 15:02:22 +0100

    Add basic boilerplate main.ts with a logger middleware and swagger

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Global, Module,MiddlewareConsumer, NestModule } from '@nestjs/common';
+import {ConfigModule} from "@nestjs/config";
+import {LoggerMiddleware} from "./utils/middlewares/logger.middleware";
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+@Global()
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
